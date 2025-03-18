acd98f781205daf8aa060c191b50b1184403d99e
nikhil <x11.niks@gmail.com>
2025-03-18 20:27:41 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String args[]) {
+         drip();
+    System.out.println("say hi");
+      
+    }
+
+
+    private static void drip() {
+        System.out.println("nikhil here");
+    }
+}
